d1e39f6511fffa278a5b094e0f096a269f38e4a8
GSmithApps <grant.smith@temporal.io>
2025-02-07 20:18:05 -0600

    started configuring the activities

--- polling-infrequent/src/workflows.ts
+++ polling-infrequent/src/workflows.ts
@@ -1,11 +1,19 @@
 // @@@SNIPSTART typescript-hello-workflow
-import { proxyActivities } from '@temporalio/workflow';
+import { proxyActivities, ActivityOptions, RetryPolicy } from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
 
-const { greet } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '1 minute',
-});
+const retryPolicy: RetryPolicy = {
+  backoffCoefficient: 1,
+  initialInterval: 60 * 1000,
+}
+
+const activityOptions: ActivityOptions = {
+  retry: retryPolicy,
+  startToCloseTimeout: 2 * 1000,
+}
+
+const { greet } = proxyActivities<typeof activities>(activityOptions);
 
 /** A workflow that simply calls an activity */
 export async function example(name: string): Promise<string> {
